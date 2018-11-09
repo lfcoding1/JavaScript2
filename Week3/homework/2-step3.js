196a154d872b83e1e35ef091b0cea23003f780df
Louise Francois <louise.ban@gmail.com>
2018-11-09 14:36:43 +0100

    change to step2

--- Week3/homework/2-step3.js
+++ Week3/homework/2-step3.js
@@ -1,31 +1,32 @@
 'use strict';
 
+function sayThree() {
+  console.log(3);
+}
+
+function sayFive() {
+  console.log(5);
+}
 
 function threeFive(startIndex, stopIndex, threeCallback, fiveCallback) {
-  const values = [];
-  for (var i = startIndex; i <= stopIndex; i++){
-    values.push(i)
-  }
-  console.log(values);
+const values = [];
+for (var i = startIndex; i <= stopIndex; i++){
+  values.push(i)
+}
+console.log(values);
 
-  for (let j = 0; j < values.length; j++){
-    if (j % 3 === 0) {
-      threeCallback("3");
-    }
-    else if (j % 5 === 0) {
-      fiveCallback("5");
-    }
-    else if ((j % 3=== 0) && (j % 5 === 0)) {
-      fiveCallback("5") && threeCallback("3");
-    }
+for (let j = 0; j < values.length; j++){
+  if ((values[j] % 3 === 0) && (values[j] % 5 === 0)) {
+          threeCallback()
+          fiveCallback();
+  }
+  else if(values[j] % 3 === 0) {
+      threeCallback();
+  }
+  else if (values[j] % 5 === 0) {
+          fiveCallback();
   }
 }
-  function sayThree(threeCallback) {
-    console.log(threeCallback);
-  }
-  
-  function sayFive(fiveCallback) {
-    console.log(fiveCallback);
-  }
+}
 
 threeFive(10, 15, sayThree, sayFive)
